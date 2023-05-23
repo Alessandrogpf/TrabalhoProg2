7d52ebecc4716cdc4385490f4c8a4750d2bd7abb
Jonathas <jonathasborges12@gmail.com>
2023-05-23 12:32:45 -0400

    Implementação de menu. Ao selecionar a op 1 e 1 de novo acontece o seguinte erro: Segmentation fault Não estou conseguindo achar o ponteiro que está causando isso.

--- Main/TrabalhoProg2.c
+++ Main/TrabalhoProg2.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 // Essa função ordena um vetor utilizando o método Selection Sort
-void selection_sort(int* vetor, int tamanho_vetor){
+int* selection_sort(int* vetor, int tamanho_vetor){
     int i, j, menor, troca;
 
     for (i = 0; i < tamanho_vetor - 1; i++){
@@ -24,27 +24,16 @@ void selection_sort(int* vetor, int tamanho_vetor){
     for (i = 0; i < tamanho_vetor; i++){
         printf("pos[%d] = %d\n", i, vetor[i]);
     }
+    return vetor;
 }
 // --------------------------------------------------------------------------------
 
-int main(){
-    int tamanho_vetor, i;
-    int* vetor;
-    printf("Digite o tamanho do vetor: ");
-    scanf("%d", &tamanho_vetor);
-    vetor = (int *) calloc(tamanho_vetor, sizeof(int));
+// Calcula desvio padrão
+int dp(){}
 
-    // Gera número aleatórios e os adiciona dentro do vetor
-    srand((unsigned) time(NULL));
-    printf("\nVetor não ordenado:\n");
-    for (i = 0; i < tamanho_vetor; i++){
-        vetor[i] = rand() % 10;
-        printf("pos[%d] = %d\n", i, vetor[i]);
-    }
-    // --------------------------------------------------------------------------------
 
-    // Usado para medição de tempo de execução
-    clock_t start, end;
+void mede_tempo(int* vetor, int tamanho_vetor){
+     clock_t start, end;
     double cpu_time_used;
 
     start = clock();
@@ -52,8 +41,94 @@ int main(){
     end = clock();
 
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("O tempo gasto para ordenar foi: %f", cpu_time_used);
-    // --------------------------------------------------------------------------------
+    printf("------------------------------------------------------");
+    printf("\n\nO tempo gasto pelo Selection Sort foi: %f\n", cpu_time_used);
+}
+
+// Gera um vetor aleatório
+int* vetor_random(int* vetor, int tamanho_vetor, int i){
+    vetor = (int *) calloc(tamanho_vetor, sizeof(int));
+    // Gera número aleatórios e os adiciona dentro do vetor
+    srand((unsigned) time(NULL));
+    printf("\nVetor não ordenado:\n");
+    for (i = 0; i < tamanho_vetor; i++){
+        vetor[i] = rand() % 10;
+        printf("pos[%d] = %d\n", i, vetor[i]);
+    }
+    return vetor;
+}
+// --------------------------------------------------------------------------------
+
+void menu(int v, int tamanho_vetor){
+    int op, cond = 1;
+
+    while(cond == 1){
+        printf("\n-------------- Bem vindo ao menu! --------------------\n");
+        printf("Esse programa realiza a ordenação de um vetor e avalia o tempo de execução\n");
+        printf("\n1 - Ordenar vetor\n");
+        printf("2 - Avaliar tempos de execução\n");
+        printf("3 - Rodar 10x e calcular o desvio padrão\n");
+        printf("4 - Alterar tamanho do vetor\n");
+        printf("5 - Gráfico de tamanho n x tempo\n");
+        printf("0 - Encerre o programa\n");
+        printf("Escolha uma das opções acima: ");
+        scanf("%d", &op);
+        printf("------------------------------------------------------");
+
+        switch (op)
+        {   
+            case 1:
+                int op_ord;
+                //printf("------------------------------------------------------");
+                printf("\n1 - Selection Sort\n");
+                printf("2 - Insertion Sort\n");
+                printf("3 - Merge Sort\n");
+                printf("4 - Quick Sort\n");
+                printf("Escolha uma das ordenações acima: ");
+                scanf("%d", &op_ord);
+                switch(op_ord)
+                {
+                    case 1:
+                        selection_sort(v, tamanho_vetor);
+                        break;
+                    // Adicionar as outras funções conforme forem implementadas;
+                }
+                break;
+
+            case 2:
+                mede_tempo(v, tamanho_vetor);
+                break;
+            // Rodar 10x e calcular o desvio padrão
+            case 3:
+                /* Código */
+                break;
+
+            case 4:
+                //printf("------------------------------------------------------");
+                printf("\nDigite o novo tamanho: ");
+                scanf("%d", &tamanho_vetor);
+                break;
+            // Gráfico
+            case 5:
+                /* Código */
+                break;
+
+            case 0:
+                printf("\nPrograma encerrado!\n");
+                cond = 0;
+        }
+    }
+}
+
+int main(){
+    int tamanho_vetor, i, v;
+    int* vetor;
+
+    printf("Por favor, digite um tamanho para o vetor: ");
+    scanf("%d", &tamanho_vetor);
+
+    v = vetor_random(vetor, tamanho_vetor, i);
+    menu(v, tamanho_vetor);
 
     return 0;
 }
